2ce0b75cba2a814289111d5039cd499cc1ba19ce
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-11-21 15:04:25 +0100

    K1J-1607: Refactor updateFromPu method in UpdatePrivatePractitionerFromPUService to streamline response handling and improve readability

--- app/src/main/java/se/inera/intyg/privatepractitionerservice/application/privatepractitioner/service/UpdatePrivatePractitionerFromPUService.java
+++ app/src/main/java/se/inera/intyg/privatepractitionerservice/application/privatepractitioner/service/UpdatePrivatePractitionerFromPUService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import se.inera.intyg.privatepractitionerservice.application.privatepractitioner.service.model.PrivatePractitioner;
 import se.inera.intyg.privatepractitionerservice.infrastructure.persistence.repository.PrivatePractitionerRepository;
 import se.inera.intyg.privatepractitionerservice.integration.api.pu.GetPersonIntegrationRequest;
-import se.inera.intyg.privatepractitionerservice.integration.api.pu.GetPersonIntegrationResponse;
 import se.inera.intyg.privatepractitionerservice.integration.api.pu.GetPersonIntegrationService;
 import se.inera.intyg.privatepractitionerservice.integration.api.pu.model.Status;
 
@@ -19,29 +18,28 @@ public class UpdatePrivatePractitionerFromPUService {
   private final PrivatePractitionerRepository privatePractitionerRepository;
 
   public PrivatePractitioner updateFromPu(PrivatePractitioner practitioner) {
-    final GetPersonIntegrationResponse response;
     try {
       final var request = new GetPersonIntegrationRequest(practitioner.getPersonId());
 
-      response = getPersonIntegrationService.getPerson(request);
+      final var response = getPersonIntegrationService.getPerson(request);
+      if (response.getStatus() != Status.FOUND) {
+        return practitioner;
+      }
+
+      final var puPerson = response.getPerson();
+      final var hasNoChanges = practitioner.getName().equals(puPerson.getName());
+
+      if (hasNoChanges) {
+        return practitioner;
+      }
+
+      practitioner.setName(puPerson.getName());
+
+      return privatePractitionerRepository.save(practitioner);
     } catch (Exception e) {
       return practitioner;
     }
 
-    if (response.getStatus() != Status.FOUND) {
-      return practitioner;
-    }
-
-    final var puPerson = response.getPerson();
-    final var hasNoChanges = practitioner.getName().equals(puPerson.getName());
-
-    if (hasNoChanges) {
-      return practitioner;
-    }
-
-    practitioner.setName(puPerson.getName());
-
-    return privatePractitionerRepository.save(practitioner);
   }
 
 }
