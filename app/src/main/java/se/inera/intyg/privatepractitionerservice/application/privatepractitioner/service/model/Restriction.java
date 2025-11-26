28d2ee9f1b213388d2538251db4267343ec3fa75
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-11-26 16:20:55 +0100

    K1J-1623: Add isRestrictedPhysician method to Restriction record

--- app/src/main/java/se/inera/intyg/privatepractitionerservice/application/privatepractitioner/service/model/Restriction.java
+++ app/src/main/java/se/inera/intyg/privatepractitionerservice/application/privatepractitioner/service/model/Restriction.java
@@ -3,4 +3,11 @@ package se.inera.intyg.privatepractitionerservice.application.privatepractitione
 public record Restriction(String restrictionCode, String restrictionName,
                           String healthCareProfessionalLicenceCode) {
 
+
+  public static final String REVOKED_LICENSE = "001";
+
+  public boolean isRestrictedPhysician() {
+    return healthCareProfessionalLicenceCode.equals("LK") && restrictionCode.equals(
+        REVOKED_LICENSE);
+  }
 }
