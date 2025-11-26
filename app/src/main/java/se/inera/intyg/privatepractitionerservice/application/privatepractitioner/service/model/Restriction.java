4720d83347185fefcaacef9923e942e9bec0cf88
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-11-26 11:42:47 +0100

    K1J-1623: Add Restriction record to model private practitioner restrictions

---
+++ app/src/main/java/se/inera/intyg/privatepractitionerservice/application/privatepractitioner/service/model/Restriction.java
@@ -0,0 +1,6 @@
+package se.inera.intyg.privatepractitionerservice.application.privatepractitioner.service.model;
+
+public record Restriction(String restrictionCode, String restrictionName,
+                          String healthCareProfessionalLicenceCode) {
+
+}
